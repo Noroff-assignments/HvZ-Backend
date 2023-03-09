4ab5bc7cf2c3b5f6d8d524957f99f321adaf645c
rene-marcker <rene-marcker@hotmail.com>
2023-03-09 15:08:55 +0100

    Feat: fillout the admin model

--- hvz-backend/Models/Admin.cs
+++ hvz-backend/Models/Admin.cs
@@ -4,6 +4,18 @@ namespace hvz_backend.Models
 {
     public class Admin
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(2), MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required, MinLength(2), MaxLength(100)]
+        public string LastName { get; set; }
+
+        public enum State
+        {
+            Active,
+            Inactive
+        }
 
     }
 }
