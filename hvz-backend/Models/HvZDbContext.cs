acec1f23e491c45eb304e98f36f0fbd6356c2e74
rene-marcker <rene-marcker@hotmail.com>
2023-03-09 14:16:08 +0100

    feat: created base DbContext and implemented in programs and appsettings.json

---
+++ hvz-backend/Models/HvZDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace hvz_backend.Models
+{
+    public class HvZDbContext : DbContext
+    {
+        // Sets the tables for the database
+
+        public HvZDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- hvz-backend/Program.cs
+++ hvz-backend/Program.cs
@@ -1,11 +1,46 @@
+using hvz_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<HvZDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle .
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Web API for Human vs. Zombies database.",
+        Description = "Database containing all the games, user, player and points of interess.",
+        Contact = new OpenApiContact
+        {
+            Name = "Rene 'Code Crusher' Marcker & Michael Piepgras Neergaard",
+            Url = new Uri("https://github.com/Noroff-assignments/hvz-backend")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT 2022",
+            Url = new Uri("https://opensource.org/license/mit/")
+        }
+    });
+    options.IncludeXmlComments(xmlPath);
+});
+
+// Adds services to the builder
 
 var app = builder.Build();
 
