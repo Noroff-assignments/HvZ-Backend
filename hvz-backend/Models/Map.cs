f8108ca2baf9b63aa417833e075f662be0d12191
rene-marcker <rene-marcker@hotmail.com>
2023-03-10 08:27:19 +0100

    Feat: implemented the model for Map

--- hvz-backend/Models/Map.cs
+++ hvz-backend/Models/Map.cs
@@ -1,7 +1,17 @@
-﻿namespace hvz_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hvz_backend.Models
 {
     public class Map
     {
+        [Key] 
+        public int Id { get; set; }
+        [Required]
+        public string MapSeed { get; set; }
+        [Required, MaxLength(50)]
+        public string MapName { get; set; }
+        [MaxLength(255)]
+        public string? MapDescription { get; set; }
 
     }
 }
