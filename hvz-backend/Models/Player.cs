6887afd5fc753a43c44db35261529efbdb12522c
rene-marcker <rene-marcker@hotmail.com>
2023-03-10 10:14:42 +0100

    Feat: implemented the model for Player

--- hvz-backend/Models/Player.cs
+++ hvz-backend/Models/Player.cs
@@ -1,7 +1,20 @@
-﻿namespace hvz_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hvz_backend.Models
 {
     public class Player
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string BiteCode { get; set; }
+
+        [Required]
+        public bool IsZombie { get; set; }
+
+        [Required]
+        public bool IsPatientZero { get; set; }
 
     }
 }
